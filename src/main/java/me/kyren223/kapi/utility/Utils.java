52d5fdb8ff232a3d53cdc20459c253668cee4e7c
Kyren223 <ulmliad223@gmail.com>
2024-05-30 00:41:04 +0300

    Committing build 1.1.0-rc3 - Added Hex support for Utils#col(String)

--- src/main/java/me/kyren223/kapi/utility/Utils.java
+++ src/main/java/me/kyren223/kapi/utility/Utils.java
@@ -41,6 +41,7 @@
 package me.kyren223.kapi.utility;
 
 import me.kyren223.kapi.annotations.Kapi;
+import me.kyren223.kapi.data.Option;
 import org.bukkit.ChatColor;
 import org.jetbrains.annotations.Contract;
 import org.jspecify.annotations.NullMarked;
@@ -60,7 +61,13 @@ public class Utils {
     }
     
     /**
-     * Replaces color codes starting with '&amp;' with their appropriate ChatColor.
+     * Formats the string to have colors.<br>
+     * Supports the following color codes:
+     * <ul>
+     *     <li>&amp; - converts to {@link ChatColor#COLOR_CHAR} which is '§' </li>
+     *     <li>#[a-fA-F0-9]{6} - converts to hex color codes</li>
+     *     <li>&# - Converts to '#', used to escape hex</li>
+     * </ul>
      *
      * @param s The string to color
      * @return The colored string
@@ -68,7 +75,54 @@ public class Utils {
     @Kapi
     @Contract(pure = true)
     public static String col(String s) {
-        return ChatColor.translateAlternateColorCodes('&', s);
+        int skip = 0;
+        StringBuilder sb = new StringBuilder();
+        outer:
+        for (int i = 0; i < s.length(); i++) {
+            if (skip > 0) {
+                skip--;
+                continue;
+            }
+            
+            char c = s.charAt(i);
+            Option<Character> nextC = i + 1 < s.length() ? Option.some(s.charAt(i + 1)) : Option.none();
+            
+            if (nextC.isSomeAnd(next -> next == '#' && c == '&')) {
+                // Escape # after & into just #
+                sb.append('#');
+                skip = 1; // Skip the next character which is the #
+                continue;
+            }
+            
+            if (c == '&') {
+                sb.append(ChatColor.COLOR_CHAR);
+                continue;
+            }
+            
+            if (c == '#') {
+                // Try parsing the hex color
+                char[] hex = new char[6];
+                for (int j = 0; j < 6; j++) {
+                    if (i + j + 1 >= s.length()) {
+                        sb.append(c);
+                        continue outer;
+                    }
+                    char next = s.charAt(i + j + 1);
+                    boolean isDigit = Character.isDigit(next);
+                    boolean isHex = 'a' <= next && next <= 'f' || 'A' <= next && next <= 'F';
+                    if (!isDigit && !isHex) {
+                        sb.append(c);
+                        continue outer;
+                    }
+                    hex[j] = s.charAt(i + j + 1);
+                }
+                String color = net.md_5.bungee.api.ChatColor.of("#" + new String(hex)) + "";
+                sb.append(color);
+            }
+            
+            sb.append(c);
+        }
+        return sb.toString();
     }
     
     /**
