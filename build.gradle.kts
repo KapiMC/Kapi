e47eb37d091ab1e3de346efbde615710bc248e36
Kyren223 <ulmliad223@gmail.com>
2024-08-26 18:50:17 +0300

    chore: updated copyright notices in each file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2024 Kyren223
+ * Licensed under the AGPLv3. See LICENSE or https://www.gnu.org/licenses/agpl-3.0 for details.
+ */
+
 plugins {
     id("maven-publish")
     id("java-library")
