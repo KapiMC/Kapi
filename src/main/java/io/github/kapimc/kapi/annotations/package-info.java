acb9ae9f000684d59b2a51dfbe4128dda8ea5d46
Kyren223 <ulmliad223@gmail.com>
2024-09-01 00:50:34 +0300

    chore: added package-info to annotations package

---
+++ src/main/java/io/github/kapimc/kapi/annotations/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024 Kyren223
+ * Licensed under the AGPLv3 license. See LICENSE or https://www.gnu.org/licenses/agpl-3.0 for details.
+ */
+
+/**
+ * This package contains annotations that are used by Kapi.
+ */
+@NullMarked
+package io.github.kapimc.kapi.annotations;
+
+import org.jspecify.annotations.NullMarked;
