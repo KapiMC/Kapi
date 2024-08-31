992a901abe054c9f0f1a2faebf60dfac122d20e1
Kyren223 <ulmliad223@gmail.com>
2024-08-31 19:18:22 +0300

    chore: added @Experimental annotation to all unstable APIs

--- src/main/java/me/kyren223/kapi/engine/ecs/events/package-info.java
+++ src/main/java/me/kyren223/kapi/engine/ecs/events/package-info.java
@@ -3,7 +3,9 @@
  * Licensed under the AGPLv3 license. See LICENSE or https://www.gnu.org/licenses/agpl-3.0 for details.
  */
 
+@ApiStatus.Experimental
 @NullMarked
 package me.kyren223.kapi.engine.ecs.events;
 
+import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
