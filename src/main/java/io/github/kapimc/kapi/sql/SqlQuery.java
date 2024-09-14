0c9540a739971466b837ac47cb8e15790f45128f
Kyren223 <ulmliad223@gmail.com>
2024-09-15 01:03:50 +0300

    feat: added SqlQuery record to hold SQL queries

---
+++ src/main/java/io/github/kapimc/kapi/sql/SqlQuery.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2024 Kyren223
+ * Licensed under the GPL-3.0 license.
+ * See https://www.gnu.org/licenses/gpl-3.0 for details.
+ * Created for Kapi: https://github.com/kapimc/kapi
+ */
+
+package io.github.kapimc.kapi.sql;
+
+import io.github.kapimc.kapi.annotations.Kapi;
+import io.github.kapimc.kapi.data.Result;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * A query to be executed on a database.
+ *
+ * @param query the query to execute
+ * @param args  the arguments to use in the query
+ */
+@Kapi
+public record SqlQuery(String query, Object... args) {
+    
+    /**
+     * Executes the query on the given database.
+     *
+     * @param db the database to execute the query on
+     * @return the result of the query
+     * @see SqliteDB#executeUpdate(String, Object...)
+     */
+    @Kapi
+    public Result<Integer,SQLException> executeUpdate(SqliteDB db) {
+        return db.executeUpdate(query, args);
+    }
+    
+    /**
+     * Executes the query on the given database.
+     *
+     * @param db the database to execute the query on
+     * @return the result of the query
+     * @see SqliteDB#executeQuery(String, Object...)
+     */
+    @Kapi
+    public Result<ResultSet,SQLException> executeQuery(SqliteDB db) {
+        return db.executeQuery(query, args);
+    }
+}
