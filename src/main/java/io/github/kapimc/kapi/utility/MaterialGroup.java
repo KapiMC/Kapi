f26d6c681dca6fa1e6f76988cd5bfcafe1492696
Imabanana80 <imabanana80.hallo@gmail.com>
2024-09-15 14:22:44 +0800

    feat: added ore MaterialGroups

--- src/main/java/io/github/kapimc/kapi/utility/MaterialGroup.java
+++ src/main/java/io/github/kapimc/kapi/utility/MaterialGroup.java
@@ -28,11 +28,10 @@ public enum MaterialGroup{
         Material.WOODEN_SWORD,
         Material.STONE_SWORD,
         Material.IRON_SWORD,
-        Material.DIAMOND_SWORD,
         Material.GOLDEN_SWORD,
+        Material.DIAMOND_SWORD,
         Material.NETHERITE_SWORD
     )),
-    
     /**
      * The pickaxes of all 5 basic materials, wood, stone, iron, gold, diamond and netherite
      */
@@ -45,7 +44,6 @@ public enum MaterialGroup{
         Material.DIAMOND_PICKAXE,
         Material.NETHERITE_PICKAXE
     )),
-    
     /**
      * The axes of all 5 basic materials, wood, stone, iron, gold, diamond and netherite
      */
@@ -58,7 +56,6 @@ public enum MaterialGroup{
         Material.DIAMOND_AXE,
         Material.NETHERITE_AXE
     )),
-    
     /**
      * The shovels of all 5 basic materials, wood, stone, iron, gold, diamond and netherite
      */
@@ -71,7 +68,6 @@ public enum MaterialGroup{
         Material.DIAMOND_SHOVEL,
         Material.NETHERITE_SHOVEL
     )),
-    
     /**
      * The hoes of all 5 basic materials, wood, stone, iron, gold, diamond and netherite
      */
@@ -83,6 +79,100 @@ public enum MaterialGroup{
         Material.GOLDEN_HOE,
         Material.DIAMOND_HOE,
         Material.NETHERITE_HOE
+    )),
+    
+    /**
+     * Both the stone and deepslate varients of all ores as well as the nether ores including ancient debris
+     */
+    @Kapi
+    ORES(Set.of(
+        Material.COAL_ORE,
+        Material.IRON_ORE,
+        Material.COPPER_ORE,
+        Material.GOLD_ORE,
+        Material.EMERALD_ORE,
+        Material.LAPIS_ORE,
+        Material.REDSTONE_ORE,
+        Material.DIAMOND_ORE,
+        Material.DEEPSLATE_COAL_ORE,
+        Material.DEEPSLATE_IRON_ORE,
+        Material.DEEPSLATE_COPPER_ORE,
+        Material.DEEPSLATE_GOLD_ORE,
+        Material.DEEPSLATE_EMERALD_ORE,
+        Material.DEEPSLATE_LAPIS_ORE,
+        Material.DEEPSLATE_REDSTONE_ORE,
+        Material.DEEPSLATE_DIAMOND_ORE,
+        Material.NETHER_GOLD_ORE,
+        Material.NETHER_QUARTZ_ORE,
+        Material.ANCIENT_DEBRIS
+    )),
+    /**
+     * Stone varients of all ores
+     */
+    @Kapi
+    STONE_ORES(Set.of(
+        Material.COAL_ORE,
+        Material.IRON_ORE,
+        Material.COPPER_ORE,
+        Material.GOLD_ORE,
+        Material.EMERALD_ORE,
+        Material.LAPIS_ORE,
+        Material.REDSTONE_ORE,
+        Material.DIAMOND_ORE
+    )),
+    /**
+     * Deepslate varients of all ores
+     */
+    @Kapi
+    DEEPSLATE_ORES(Set.of(
+        Material.DEEPSLATE_COAL_ORE,
+        Material.DEEPSLATE_IRON_ORE,
+        Material.DEEPSLATE_COPPER_ORE,
+        Material.DEEPSLATE_GOLD_ORE,
+        Material.DEEPSLATE_EMERALD_ORE,
+        Material.DEEPSLATE_LAPIS_ORE,
+        Material.DEEPSLATE_REDSTONE_ORE,
+        Material.DEEPSLATE_DIAMOND_ORE
+    )),
+    /**
+     * All ores that naturally generate in the nether
+     */
+    @Kapi
+    NETHER_ORES(Set.of(
+        Material.NETHER_GOLD_ORE,
+        Material.NETHER_QUARTZ_ORE,
+        Material.ANCIENT_DEBRIS
+    )),
+    /**
+     * ALl ores that typically require smelting to obtain the useful product or drop raw varients
+     */
+    @Kapi
+    SMELTABLE_ORES(Set.of(
+        Material.IRON_ORE,
+        Material.COPPER_ORE,
+        Material.GOLD_ORE,
+        Material.DEEPSLATE_IRON_ORE,
+        Material.DEEPSLATE_COPPER_ORE,
+        Material.DEEPSLATE_GOLD_ORE,
+        Material.NETHER_GOLD_ORE,
+        Material.NETHER_QUARTZ_ORE,
+        Material.ANCIENT_DEBRIS
+    )),
+    /**
+     * All ores that typically do not require smelting to obtain the useful product and do not drop raw varients
+     */
+    @Kapi
+    UNSMELTABLE_ORES(Set.of(
+        Material.COAL_ORE,
+        Material.EMERALD_ORE,
+        Material.LAPIS_ORE,
+        Material.REDSTONE_ORE,
+        Material.DIAMOND_ORE,
+        Material.DEEPSLATE_COAL_ORE,
+        Material.DEEPSLATE_EMERALD_ORE,
+        Material.DEEPSLATE_LAPIS_ORE,
+        Material.DEEPSLATE_REDSTONE_ORE,
+        Material.DEEPSLATE_DIAMOND_ORE
     ));
     
     private final Set<Material> materials;
