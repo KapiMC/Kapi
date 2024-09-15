a6b04e7f626059e9cd94b7bd236d48fa791f7738
Imabanana80 <imabanana80.hallo@gmail.com>
2024-09-15 14:30:42 +0800

    feat: added tool and weapon MaterialGroups

--- src/main/java/io/github/kapimc/kapi/utility/MaterialGroup.java
+++ src/main/java/io/github/kapimc/kapi/utility/MaterialGroup.java
@@ -80,6 +80,84 @@ public enum MaterialGroup{
         Material.DIAMOND_HOE,
         Material.NETHERITE_HOE
     )),
+    /**
+     * All the basic tools not including swords
+     */
+    @Kapi
+    TOOLS(Set.of(
+        Material.WOODEN_PICKAXE,
+        Material.STONE_PICKAXE,
+        Material.IRON_PICKAXE,
+        Material.GOLDEN_PICKAXE,
+        Material.DIAMOND_PICKAXE,
+        Material.NETHERITE_PICKAXE,
+        Material.WOODEN_AXE,
+        Material.STONE_AXE,
+        Material.IRON_AXE,
+        Material.GOLDEN_AXE,
+        Material.DIAMOND_AXE,
+        Material.NETHERITE_AXE,
+        Material.WOODEN_SHOVEL,
+        Material.STONE_SHOVEL,
+        Material.IRON_SHOVEL,
+        Material.GOLDEN_SHOVEL,
+        Material.DIAMOND_SHOVEL,
+        Material.NETHERITE_SHOVEL,
+        Material.WOODEN_HOE,
+        Material.STONE_HOE,
+        Material.IRON_HOE,
+        Material.GOLDEN_HOE,
+        Material.DIAMOND_HOE,
+        Material.NETHERITE_HOE
+    )),
+    @Kapi
+    WEAPONS(Set.of(
+        Material.WOODEN_SWORD,
+        Material.STONE_SWORD,
+        Material.IRON_SWORD,
+        Material.GOLDEN_SWORD,
+        Material.DIAMOND_SWORD,
+        Material.NETHERITE_SWORD,
+        Material.WOODEN_AXE,
+        Material.STONE_AXE,
+        Material.IRON_AXE,
+        Material.GOLDEN_AXE,
+        Material.DIAMOND_AXE,
+        Material.NETHERITE_AXE,
+        Material.TRIDENT,
+        Material.BOW,
+        Material.CROSSBOW
+        // TODO: add mace when updating to 1.21+
+    )),
+    /**
+     * Weapons that are used to deal damage within a player's reach distance
+     */
+    @Kapi
+    MELEE_WEAPONS(Set.of(
+        Material.WOODEN_SWORD,
+        Material.STONE_SWORD,
+        Material.IRON_SWORD,
+        Material.GOLDEN_SWORD,
+        Material.DIAMOND_SWORD,
+        Material.NETHERITE_SWORD,
+        Material.WOODEN_AXE,
+        Material.STONE_AXE,
+        Material.IRON_AXE,
+        Material.GOLDEN_AXE,
+        Material.DIAMOND_AXE,
+        Material.NETHERITE_AXE,
+        Material.TRIDENT
+        // TODO: add mace when updating to 1.21+
+    )),
+    /**
+     * Weapons that launch (as) projectiles and are more effective at range
+     */
+    @Kapi
+    RANGED_WEAPONS(Set.of(
+        Material.BOW,
+        Material.CROSSBOW,
+        Material.TRIDENT
+    )),
     
     /**
      * Both the stone and deepslate varients of all ores as well as the nether ores including ancient debris
