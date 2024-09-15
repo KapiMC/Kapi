52923adc7531ad5db5e4f763cf5c113b2119a933
Kyren223 <ulmliad223@gmail.com>
2024-09-15 11:55:17 +0300

    feat: added a SQL query builder for CREATE TABLE

--- src/main/java/io/github/kapimc/kapi/sql/QueryBuilder.java
+++ src/main/java/io/github/kapimc/kapi/sql/QueryBuilder.java
@@ -7,19 +7,44 @@
 
 package io.github.kapimc.kapi.sql;
 
+import io.github.kapimc.kapi.annotations.Kapi;
+
 import java.util.List;
 
-public class QueryBuilder<T extends QueryBuilder<T>> {
+public abstract class QueryBuilder<T extends QueryBuilder<T>> {
+    
+    /**
+     * A list of types that should be quoted.
+     */
+    @Kapi
+    public static final List<String> QUOTED_TYPES = List.of("TEXT", "CHAR", "VARCHAR");
+    
     
     protected final StringBuilder sql = new StringBuilder();
-    
-    private static final List<String> QUOTED_TYPES = List.of("TEXT", "CHAR", "VARCHAR");
     
     @SuppressWarnings("unchecked")
     protected T self() {
         return (T) this;
     }
     
+    /**
+     * Builds the QueryBuilder into a SQL query.
+     *
+     * @return the built SQL query
+     */
+    @Kapi
+    public abstract SqlQuery build();
+    
+    /**
+     * Appends raw SQL to the query.
+     * <p>
+     * This should only be used when necessary,
+     * as it can lead to SQL injection vulnerabilities.
+     *
+     * @param raw the raw SQL to append
+     * @return this, for chaining
+     */
+    @Kapi
     public T raw(String raw) {
         sql.append(raw);
         return self();
@@ -29,20 +54,49 @@ public class QueryBuilder<T extends QueryBuilder<T>> {
         return new SelectQueryBuilder();
     }
     
-    public static CreateTableQueryBuilder createTable(String name) {
-        return new CreateTableQueryBuilder(name);
+    /**
+     * Creates a query builder for creating a SQL table.
+     *
+     * @param table the name of the table
+     * @return a new CreateTableQueryBuilder
+     */
+    @Kapi
+    public static CreateTableQueryBuilder createTable(String table) {
+        return new CreateTableQueryBuilder(table);
     }
     
     public static class SelectQueryBuilder extends QueryBuilder<SelectQueryBuilder> {
     
     }
     
+    /**
+     * A query builder for creating SQL tables.
+     */
+    @Kapi
     public static class CreateTableQueryBuilder extends QueryBuilder<CreateTableQueryBuilder> {
         
         private CreateTableQueryBuilder(String name) {
             sql.append("CREATE TABLE IF NOT EXISTS ").append(name).append(" (");
         }
         
+        /**
+         * {@inheritDoc}
+         */
+        @Kapi
+        @Override
+        public SqlQuery build() {
+            sql.append(");");
+            return new SqlQuery(sql.toString());
+        }
+        
+        /**
+         * Adds a column to the table.
+         *
+         * @param name the name of the column
+         * @param type the type of the column
+         * @return this, for chaining
+         */
+        @Kapi
         public CreateTableQueryBuilder column(String name, String type) {
             if (sql.charAt(sql.length() - 1) != '(') {
                 sql.append(", ");
@@ -51,6 +105,18 @@ public class QueryBuilder<T extends QueryBuilder<T>> {
             return this;
         }
         
+        /**
+         * Adds a column to the table with a default value.
+         * <p>
+         * The default value will be quoted if the type requires it.
+         * Supported types are {@link QueryBuilder#QUOTED_TYPES}.
+         *
+         * @param name         the name of the column
+         * @param type         the type of the column
+         * @param defaultValue the default value of the column
+         * @return this, for chaining
+         */
+        @Kapi
         public CreateTableQueryBuilder column(String name, String type, String defaultValue) {
             column(name, type);
             sql.append(" DEFAULT ");
@@ -62,10 +128,27 @@ public class QueryBuilder<T extends QueryBuilder<T>> {
             return this;
         }
         
+        /**
+         * Adds an auto-incrementing integer primary key to the table.
+         *
+         * @param name the name of the column
+         * @return this, for chaining
+         */
+        @Kapi
         public CreateTableQueryBuilder columnAutoPrimaryKey(String name) {
             return column(name, "INTEGER").primaryKey().raw(" AUTOINCREMENT");
         }
         
+        /**
+         * Adds a primary key to the table.
+         * <p>
+         * If no columns are provided, it is assumed that the primary key is applied to the column.
+         * If one or more columns are provided, it is assumed that the primary key is applied to the table.
+         *
+         * @param columns the columns to add as a primary key
+         * @return this, for chaining
+         */
+        @Kapi
         public CreateTableQueryBuilder primaryKey(String... columns) {
             if (columns.length == 0) {
                 // On column
@@ -82,6 +165,16 @@ public class QueryBuilder<T extends QueryBuilder<T>> {
             return this;
         }
         
+        /**
+         * Adds a unique constraint to the table.
+         * <p>
+         * If no columns are provided, it is assumed that the unique constraint is applied to the column.
+         * If one or more columns are provided, it is assumed that the unique constraint is applied to the table.
+         *
+         * @param columns the columns to add as a unique constraint
+         * @return this, for chaining
+         */
+        @Kapi
         public CreateTableQueryBuilder unique(String... columns) {
             if (columns.length == 0) {
                 // On column
@@ -98,21 +191,50 @@ public class QueryBuilder<T extends QueryBuilder<T>> {
             return this;
         }
         
+        /**
+         * Adds a check constraint to the column.
+         *
+         * @param condition the condition to check
+         * @return this, for chaining
+         */
+        @Kapi
         public CreateTableQueryBuilder check(String condition) {
             sql.append(" CHECK (").append(condition).append(")");
             return this;
         }
         
+        /**
+         * Adds a check constraint to the table.
+         *
+         * @param condition the condition to check
+         * @return this, for chaining
+         */
+        @Kapi
         public CreateTableQueryBuilder checkTable(String condition) {
             sql.append(", CHECK (").append(condition).append(")");
             return this;
         }
         
+        /**
+         * Adds a not null constraint to the column.
+         *
+         * @return this, for chaining
+         */
+        @Kapi
         public CreateTableQueryBuilder notNull() {
             sql.append(" NOT NULL");
             return this;
         }
         
+        /**
+         * Adds a foreign key to the column.
+         *
+         * @param column      the column to add the foreign key to
+         * @param table       the referenced table name
+         * @param otherColumn the referenced column name in the referenced table
+         * @return this, for chaining
+         */
+        @Kapi
         public CreateTableQueryBuilder foreignKey(String column, String table, String otherColumn) {
             sql.append(", FOREIGN KEY (")
                 .append(column)
@@ -124,11 +246,29 @@ public class QueryBuilder<T extends QueryBuilder<T>> {
             return this;
         }
         
+        /**
+         * Adds an ON DELETE action to the foreign key.
+         * <p>
+         * This should only be used immediately after {@link #foreignKey(String, String, String)}.
+         *
+         * @param action the action to take when the referenced column is deleted
+         * @return this, for chaining
+         */
+        @Kapi
         public CreateTableQueryBuilder onDelete(String action) {
             sql.append(" ON DELETE ").append(action);
             return this;
         }
         
+        /**
+         * Adds an ON UPDATE action to the foreign key.
+         * <p>
+         * This should only be used immediately after {@link #foreignKey(String, String, String)}.
+         *
+         * @param action the action to take when the referenced column is updated
+         * @return this, for chaining
+         */
+        @Kapi
         public CreateTableQueryBuilder onUpdate(String action) {
             sql.append(" ON UPDATE ").append(action);
             return this;
